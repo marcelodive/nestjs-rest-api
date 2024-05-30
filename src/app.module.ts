79b511119e81032199428036664bbe6c513a593f
Marcelo Rodrigues <mrodrigues@fullstacklabs.co>
2024-05-30 20:38:57 -0300

    Adding database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASS'),
+        database: configService.get('DB_SCHEMA'),
+        entities: [],
+        synchronize: configService.get('DB_SCHEMA') === 'local',
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
